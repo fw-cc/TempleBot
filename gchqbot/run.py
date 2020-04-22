d2960b7931c5651a2b8c67bf856703a7a6a673f7
fwf <ffillingham@gmail.com>
2020-04-22 22:55:42 +0100

    Improve missing token handling

--- gchqbot/run.py
+++ gchqbot/run.py
@@ -10,13 +10,11 @@ if __name__ == "__main__":
     if not os.path.exists("./token.yml:"):
         shutil.copyfile("./token.example.yml", "./token.yml")
         print("Define token before running")
-        input()
         exit(-1)
     with open("./token.yml", "r") as token_file:
         token = yaml.safe_load(token_file)["token"]
-        if token == "your_token_here":
+        if token == "":
             print("Define token before running")
-            input()
             exit(-1)
     bot = GCHQBot(config_values["command_prefix"], base_config_options=config_values)
     bot.run(token)
