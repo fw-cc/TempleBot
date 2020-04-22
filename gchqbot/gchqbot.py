3799647e1af9b13edf3a245d6a3feaa097b452c2
fwf <ffillingham@gmail.com>
2020-04-22 21:44:15 +0100

    Update rewrite branch

--- gchqbot/gchqbot.py
+++ gchqbot/gchqbot.py
@@ -2,7 +2,8 @@ import asyncio
 import logging
 import datetime
 import os
-import motor
+from motor import motor_asyncio
+from pymongo import errors as pymongoerrors
 
 from logging import handlers
 
@@ -13,23 +14,36 @@ import discord
 class GCHQBot(commands.Bot):
     def __init__(self, command_prefix, base_config_options, **options):
         super().__init__(command_prefix, **options)
-        # self.db_conn = motor.
+        self.logger = self.__config_logging(
+            logging_level=logging.getLevelName(base_config_options["logging_level"].upper()))
+        self.logger.info(f"Command prefix: {command_prefix}")
+        self.db_client = self.__get_db_client(os.getenv("MONGOD_UNAME"), os.getenv("MONGOD_UPASS"))
         self.start_datetime = datetime.datetime.now()
-        self.logger = self.__config_logging()
 
-    def __config_logging(self, logging_level=logging.INFO, outdir="./logs"):
-        logger = logging.getLogger(__name__)
+    def __get_db_client(self, uname, upass):
+        self.logger.debug("Authenticating with mongodb instance")
+        try:
+            client_inst = motor_asyncio.AsyncIOMotorClient(username=uname, password=upass, connect=True)
+            self.logger.debug("Authentication success")
+            return client_inst
+        except pymongoerrors.PyMongoError as error:
+            self.logger.exception(f"Exception occurred while attempting to connect to mongodb:\n\n{error}")
+            return None
+
+    @staticmethod
+    def __config_logging(logging_level=logging.INFO, outdir="./logs"):
+        logger = logging.getLogger("GCHQBot")
         logger.setLevel(logging_level)
         formatter = logging.Formatter('[{asctime}] [{levelname:}] {name}: {message}',
                                       '%Y-%m-%d %H:%M:%S', style='{')
-        file_log = handlers.RotatingFileHandler(f'{outdir}/{self.start_datetime.strftime("%Y%m%d_%H%M%S")}.log',
-                                                encoding="utf-8", mode="a", backupCount=3, maxBytes=10000000)
+        # file_log = handlers.RotatingFileHandler(f'{outdir}/{self.start_datetime.strftime("%Y%m%d_%H%M%S")}.log',
+        #                                         encoding="utf-8", mode="a", backupCount=3, maxBytes=10000000)
         console_log = logging.StreamHandler()
-        file_log.setFormatter(formatter)
+        # file_log.setFormatter(formatter)
         console_log.setFormatter(formatter)
-        logger.addHandler(file_log)
+        # logger.addHandler(file_log)
         logger.addHandler(console_log)
-        logger.debug("Logging configured.")
+        logger.debug("Logging configured")
         return logger
 
 
--- gchqbot/run.py
+++ gchqbot/run.py
@@ -6,12 +6,12 @@ import os
 
 if __name__ == "__main__":
     with open("./base_config.yml", "r") as base_config:
-        config_values = yaml.load(base_config)
+        config_values = yaml.safe_load(base_config)
     with open("./token.yml", "r") as token_file:
-        token = yaml.load(token_file)["token"]
+        token = yaml.safe_load(token_file)["token"]
         if token == "your_token_here":
             print("Define token before running")
             input()
             exit(-1)
     bot = GCHQBot(config_values["command_prefix"], base_config_options=config_values)
-    bot.login(token)
+    bot.run(token)
