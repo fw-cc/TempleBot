9495984a1f181650d1f81aa253efc5aee7e4beb3
fwf <ffillingham@gmail.com>
2020-04-22 22:33:46 +0100

    Handle missing token.yml

--- gchqbot/run.py
+++ gchqbot/run.py
@@ -1,12 +1,17 @@
 from gchqbot import GCHQBot
 import yaml
-import time
 import os
+import shutil
 
 
 if __name__ == "__main__":
     with open("./base_config.yml", "r") as base_config:
         config_values = yaml.safe_load(base_config)
+    if not os.path.exists("./token.yml:"):
+        shutil.copyfile("./token.example.yml", "token.yml")
+        print("Define token before running")
+        input()
+        exit(-1)
     with open("./token.yml", "r") as token_file:
         token = yaml.safe_load(token_file)["token"]
         if token == "your_token_here":
