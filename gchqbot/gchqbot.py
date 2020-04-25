946d0f93900fc4b80ddf6664f5c5644ef81a853f
fwf <ffillingham@gmail.com>
2020-04-25 01:38:09 +0100

    Add recaptcha variable handling, Move db connection initialisation

--- gchqbot/gchqbot.py
+++ gchqbot/gchqbot.py
@@ -12,25 +12,18 @@ import discord
 
 
 class GCHQBot(commands.Bot):
-    def __init__(self, command_prefix, base_config_options, **options):
+    def __init__(self, command_prefix, base_config_options, captcha_keypair, **options):
         super().__init__(command_prefix, **options)
         self.start_datetime = datetime.datetime.now()
+        self.verification_domain = base_config_options["verification_domain"]
+        self.recaptcha_keypair = captcha_keypair
+        self.config_data = {"base": base_config_options, "captcha": captcha_keypair}
         self.logger = self.__config_logging(
             logging_level=logging.getLevelName(base_config_options["logging_level"].upper()))
         self.logger.info(f"Command prefix: {command_prefix}")
-        self.db_client = self.__get_db_client(os.getenv("MONGOD_UNAME"), os.getenv("MONGOD_UPASS"))
+        self.db_client = None
         for extension in base_config_options["extensions"]:
             self.load_extension(extension)
-
-    def __get_db_client(self, uname, upass):
-        self.logger.debug("Authenticating with mongodb instance")
-        try:
-            client_inst = motor_asyncio.AsyncIOMotorClient(username=uname, password=upass, connect=True)
-            self.logger.debug("Authentication success")
-            return client_inst
-        except pymongoerrors.PyMongoError as error:
-            self.logger.exception(f"Exception occurred while attempting to connect to mongodb:\n\n{error}")
-            return None
 
     @staticmethod
     def __config_logging(logging_level=logging.INFO, outdir="./logs"):
