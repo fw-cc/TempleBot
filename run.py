741f6168f75f0ebc3e5a63ca00bdf924b7eb2f28
fwf <ffillingham@gmail.com>
2020-06-17 23:02:19 +0100

    Add support for example base config

--- run.py
+++ run.py
@@ -6,8 +6,12 @@ import shutil
 
 
 if __name__ == "__main__":
-    with open("./templebot/base_config.yml", "r") as base_config:
-        config_values = yaml.safe_load(base_config)
+    if not os.path.exists("./templebot/base_config.yml"):
+        shutil.copyfile("./templebot/base_config.example.yml", "./templebot/base_config.yml")
+        print("Define base config options")
+    else:
+        with open("./templebot/base_config.yml", "r") as base_config:
+            config_values = yaml.safe_load(base_config)
     if not os.path.exists("./templebot/token.yml"):
         shutil.copyfile("./templebot/token.example.yml", "./templebot/token.yml")
         print("Define token before running")
